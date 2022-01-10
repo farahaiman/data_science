19d6789812bfa2db894b4fd406460ded2b49e1cc
farah <farahaiman548@gmail.com>
2022-01-10 23:30:02 +0500

    Q2 done

--- assignment1.py
+++ assignment1.py
@@ -7,5 +7,13 @@
 # 	         Like a diamond in the sky.
 # 	 Twinkle, twinkle, little star, 
 # 	         How I wonder what you are!"
-print("Twinkle, Twinkle, little star, \n\t How I wonder what you are!\n\t\t  Up above the world so high,\n\t\t  Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\t How I wonder what you are! ")
 
+#print("Twinkle, Twinkle, little star, \n\t How I wonder what you are!\n\t\t  Up above the world so high,\n\t\t  Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\t How I wonder what you are! ")
+
+#Q2 write a python program to get the python version you are using?
+
+# import sys
+# print("python version")
+# print(sys.version)
+# print("python info")
+# print(sys.version_info)
