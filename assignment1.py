5532d3f8dab8031cf5ba6f09e5972401f50625ad
farah <farahaiman548@gmail.com>
2022-01-10 23:45:16 +0500

    Q3 done

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,6 @@
-#   Assignment1
-#  Q1. write a python program to print the following string in a specific format
-#  (see the out output)
+                             #   Assignment1
+             #  Q1. write a python program to print the following string in a specific format
+             #  (see the out output)
 #       "Twinkle, Twinkle, little star,
 #           How I wonder what you are!
 # 	         Up above the world so high,
@@ -10,10 +10,17 @@
 
 #print("Twinkle, Twinkle, little star, \n\t How I wonder what you are!\n\t\t  Up above the world so high,\n\t\t  Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\t How I wonder what you are! ")
 
-#Q2 write a python program to get the python version you are using?
+          #Q2 write a python program to get the python version you are using?
 
 # import sys
 # print("python version")
 # print(sys.version)
 # print("python info")
 # print(sys.version_info)
+                  #Q3 write a python program to display the current date and time
+# import datetime
+# print(" current date and time ,") 
+# timedate =datetime.datetime.now()
+# print("time and date : ")
+# print(timedate.strftime("%Y-%m-%d %h:%M:%S"))
+
